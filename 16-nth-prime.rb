0ed3fcb791e34928301dd332022a1fc390344d2a
gilmoursa <gilmoursa@gmail.com>
2015-04-02 15:51:40 +0900

    added another problem
    added another problem

---
+++ 16-nth-prime.rb
@@ -0,0 +1,45 @@
+# Write a method that returns the `n`th prime number. Recall that only
+# numbers greater than 1 can be prime.
+#
+# Difficulty: medium.
+
+# You may use our `is_prime?` solution.
+def is_prime?(number)
+  if number <= 1
+    # only numbers > 1 can be prime.
+    return false
+  end
+
+  i = 2
+  while i < Math.sqrt(number).to_i + 1
+    if number % i == 0
+      return false
+    end
+    i+=1
+  end
+  return true
+end
+
+def nth_prime(n)
+  i = 2
+  count=0
+  while true
+    if is_prime?(i)
+      count+=1
+      if count == n
+      return i
+      end
+    end
+    i+=1
+  end
+
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts('nth_prime(1) == 2: ' + (nth_prime(1) == 2).to_s)
+puts('nth_prime(2) == 3: ' + (nth_prime(2) == 3).to_s)
+puts('nth_prime(3) == 5: ' + (nth_prime(3) == 5).to_s)
+puts('nth_prime(4) == 7: ' + (nth_prime(4) == 7).to_s)
+puts('nth_prime(5) == 11: ' + (nth_prime(5) == 11).to_s)
